7938575a5a4d0331e445373f86e526f1b4bef81f
Janice Darikho <janicedarikho@gmail.com>
2018-06-05 01:58:27 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -68,12 +68,12 @@ class Owner
   end
   
   def sell_pets
-    @pets.each do |species, animals|
+    @pets.each { |species, animals|
       animals.each do |animal|
         animal.mood = "nervous"
       end
       animals.clear
-    end
+    }
     @pets.clear
   end
   
